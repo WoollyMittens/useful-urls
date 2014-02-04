233c6ae99df9d49b05257f5cd929228bfd050271
Maurice van Creij <maurice.vancreij@webqem.com>
2014-02-05 08:17:43 +1100

    Switched to grunt.js for pre-processing.

---
+++ js/useful-urls.js
@@ -0,0 +1,305 @@
+/*
+	Source:
+	van Creij, Maurice (2012). "useful.polyfills.js: A library of useful polyfills to ease working with HTML5 in legacy environments.", version 20121126, http://www.woollymittens.nl/.
+
+	License:
+	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+*/
+
+(function (useful) {
+
+	// Invoke strict mode
+	"use strict";
+
+	// private functions
+	var polyfills = polyfills || {};
+
+	// enabled the use of HTML5 elements in Internet Explorer
+	polyfills.html5 = function () {
+		var a, b, elementsList;
+		elementsList = ['section', 'nav', 'article', 'aside', 'hgroup', 'header', 'footer', 'dialog', 'mark', 'dfn', 'time', 'progress', 'meter', 'ruby', 'rt', 'rp', 'ins', 'del', 'figure', 'figcaption', 'video', 'audio', 'source', 'canvas', 'datalist', 'keygen', 'output', 'details', 'datagrid', 'command', 'bb', 'menu', 'legend'];
+		if (navigator.userAgent.match(/msie/gi)) {
+			for (a = 0 , b = elementsList.length; a < b; a += 1) {
+				document.createElement(elementsList[a]);
+			}
+		}
+	};
+
+	// allow array.indexOf in older browsers
+	polyfills.arrayIndexOf = function () {
+		if (!Array.prototype.indexOf) {
+			Array.prototype.indexOf = function (obj, start) {
+				for (var i = (start || 0), j = this.length; i < j; i += 1) {
+					if (this[i] === obj) { return i; }
+				}
+				return -1;
+			};
+		}
+	};
+
+	// allow document.querySelectorAll (https://gist.github.com/connrs/2724353)
+	polyfills.querySelectorAll = function () {
+		if (!document.querySelectorAll) {
+			document.querySelectorAll = function (a) {
+				var b = document, c = b.documentElement.firstChild, d = b.createElement("STYLE");
+				return c.appendChild(d), b.__qsaels = [], d.styleSheet.cssText = a + "{x:expression(document.__qsaels.push(this))}", window.scrollBy(0, 0), b.__qsaels;
+			};
+		}
+	};
+
+	// allow addEventListener (https://gist.github.com/jonathantneal/3748027)
+	polyfills.addEventListener = function () {
+		!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+			WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+				var target = this;
+				registry.unshift([target, type, listener, function (event) {
+					event.currentTarget = target;
+					event.preventDefault = function () { event.returnValue = false; };
+					event.stopPropagation = function () { event.cancelBubble = true; };
+					event.target = event.srcElement || target;
+					listener.call(target, event);
+				}]);
+				this.attachEvent("on" + type, registry[0][3]);
+			};
+			WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
+				for (var index = 0, register; register = registry[index]; ++index) {
+					if (register[0] == this && register[1] == type && register[2] == listener) {
+						return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+					}
+				}
+			};
+			WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+				return this.fireEvent("on" + eventObject.type, eventObject);
+			};
+		})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
+	};
+
+	// allow console.log
+	polyfills.consoleLog = function () {
+		if (!window.console) {
+			window.console = {};
+			window.console.log = function () {
+				// if the reporting panel doesn't exist
+				var a, b, messages = '', reportPanel = document.getElementById('reportPanel');
+				if (!reportPanel) {
+					// create the panel
+					reportPanel = document.createElement('DIV');
+					reportPanel.id = 'reportPanel';
+					reportPanel.style.background = '#fff none';
+					reportPanel.style.border = 'solid 1px #000';
+					reportPanel.style.color = '#000';
+					reportPanel.style.fontSize = '12px';
+					reportPanel.style.padding = '10px';
+					reportPanel.style.position = (navigator.userAgent.indexOf('MSIE 6') > -1) ? 'absolute' : 'fixed';
+					reportPanel.style.right = '10px';
+					reportPanel.style.bottom = '10px';
+					reportPanel.style.width = '180px';
+					reportPanel.style.height = '320px';
+					reportPanel.style.overflow = 'auto';
+					reportPanel.style.zIndex = '100000';
+					reportPanel.innerHTML = '&nbsp;';
+					// store a copy of this node in the move buffer
+					document.body.appendChild(reportPanel);
+				}
+				// truncate the queue
+				var reportString = (reportPanel.innerHTML.length < 1000) ? reportPanel.innerHTML : reportPanel.innerHTML.substring(0, 800);
+				// process the arguments
+				for (a = 0, b = arguments.length; a < b; a += 1) {
+					messages += arguments[a] + '<br/>';
+				}
+				// output the queue to the panel
+				reportPanel.innerHTML = messages + reportString;
+			};
+		}
+	};
+
+	// allows Object.create (https://gist.github.com/rxgx/1597825)
+	polyfills.objectCreate = function () {
+		if (typeof Object.create !== "function") {
+			Object.create = function (original) {
+				function Clone() {}
+				Clone.prototype = original;
+				return new Clone();
+			};
+		}
+	};
+
+	// allows String.trim (https://gist.github.com/eliperelman/1035982)
+	polyfills.stringTrim = function () {
+		if (!String.prototype.trim) {
+			String.prototype.trim = function () { return this.replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, ''); };
+		}
+		if (!String.prototype.ltrim) {
+			String.prototype.ltrim = function () { return this.replace(/^\s+/, ''); };
+		}
+		if (!String.prototype.rtrim) {
+			String.prototype.rtrim = function () { return this.replace(/\s+$/, ''); };
+		}
+		if (!String.prototype.fulltrim) {
+			String.prototype.fulltrim = function () { return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, '').replace(/\s+/g, ' '); };
+		}
+	};
+
+	// for immediate use
+	polyfills.html5();
+	polyfills.arrayIndexOf();
+	polyfills.querySelectorAll();
+	polyfills.addEventListener();
+	polyfills.consoleLog();
+	polyfills.objectCreate();
+	polyfills.stringTrim();
+
+}(window.useful = window.useful || {}));
+
+/*
+	Source:
+	van Creij, Maurice (2012). "useful.urls.js: A library of useful functions to ease working with URL query parameters.", version 20121126, http://www.woollymittens.nl/.
+
+	License:
+	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+*/
+
+(function (useful) {
+
+	// Invoke strict mode
+	"use strict";
+
+	// private functions
+	var urls = urls || {};
+
+	// retrieves the query parameters from an url
+	urls.load = function (url) {
+		var a, b, parts = [], data = {}, namevalue, value;
+		parts = url.split('#')[0].replace('?', '&').split('&');
+		for (a = 1, b = parts.length; a < b; a += 1) {
+			namevalue = parts[a].split('=');
+			value = parseFloat(namevalue[1]);
+			data[namevalue[0]] = (!isNaN(value)) ? value : namevalue[1];
+		}
+		return data;
+	};
+
+	// stores query parameters to an url
+	urls.save = function (url, data) {
+		var name;
+		// clean the url
+		url = url.split('?')[0].split('#')[0];
+		// for all name value pairs
+		for (name in data) {
+			if (data.hasOwnProperty(name)) {
+				// add them to the url
+				url += '&' + name + '=' + data[name];
+			}
+		}
+		// make sure the first value starts with a ?
+		return url.replace('&', '?');
+	};
+
+	// replace a value in a query parameter
+	urls.replace = function (url, name, value) {
+		var old, match, nameValue;
+		// if the value is present in the url
+		match = new RegExp(name + '=', 'gi');
+		if (match.test(url)) {
+			// isolate the old value
+			old  = url.split('#')[0].split(name + '=')[1].split('&')[0];
+			// insert the new value
+			return url.replace(name + '=' + old, name + '=' + value);
+		} else {
+			// add the value instead of replacing it
+			nameValue = urls.load(url);
+			nameValue[name] = value;
+			return urls.save(url, nameValue);
+		}
+	};
+
+	// source - http://phpjs.org/functions/base64_encode/
+	// http://kevin.vanzonneveld.net
+	// +  original by: Tyler Akins (http://rumkin.com)
+	// +  improved by: Bayron Guevara
+	// +  improved by: Thunder.m
+	// +  improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +  bugfixed by: Pellentesque Malesuada
+	// +  improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +  improved by: Rafał Kukawski (http://kukawski.pl)
+	// *   example 1: base64_encode('Kevin van Zonneveld');
+	// *   returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+	urls.encode = function (data) {
+		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+		var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+			ac = 0,
+			enc = "",
+			tmpArr = [];
+		if (!data) {
+			return data;
+		}
+		do { // pack three octets into four hexets
+			o1 = data.charCodeAt(i++);
+			o2 = data.charCodeAt(i++);
+			o3 = data.charCodeAt(i++);
+			bits = o1 << 16 | o2 << 8 | o3;
+			h1 = bits >> 18 & 0x3f;
+			h2 = bits >> 12 & 0x3f;
+			h3 = bits >> 6 & 0x3f;
+			h4 = bits & 0x3f;
+			// use hexets to index into b64, and append result to encoded string
+			tmpArr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+		} while (i < data.length);
+		enc = tmpArr.join('');
+		var r = data.length % 3;
+		return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+	};
+
+	// source - http://phpjs.org/functions/base64_decode/
+	// http://kevin.vanzonneveld.net
+	// +  original by: Tyler Akins (http://rumkin.com)
+	// +  improved by: Thunder.m
+	// +   input by: Aman Gupta
+	// +  improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +  bugfixed by: Onno Marsman
+	// +  bugfixed by: Pellentesque Malesuada
+	// +  improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +   input by: Brett Zamir (http://brett-zamir.me)
+	// +  bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// *   example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
+	// *   returns 1: 'Kevin van Zonneveld'
+	urls.decode = function (data) {
+		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+		var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+			ac = 0,
+			dec = "",
+			tmpArr = [];
+		if (!data) {
+			return data;
+		}
+		data += '';
+		do { // unpack four hexets into three octets using index points in b64
+			h1 = b64.indexOf(data.charAt(i++));
+			h2 = b64.indexOf(data.charAt(i++));
+			h3 = b64.indexOf(data.charAt(i++));
+			h4 = b64.indexOf(data.charAt(i++));
+			bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+			o1 = bits >> 16 & 0xff;
+			o2 = bits >> 8 & 0xff;
+			o3 = bits & 0xff;
+			if (h3 == 64) {
+				tmpArr[ac++] = String.fromCharCode(o1);
+			} else if (h4 == 64) {
+				tmpArr[ac++] = String.fromCharCode(o1, o2);
+			} else {
+				tmpArr[ac++] = String.fromCharCode(o1, o2, o3);
+			}
+		} while (i < data.length);
+		dec = tmpArr.join('');
+		return dec;
+	};
+
+	// public functions
+	useful.urls = useful.urls || {};
+	useful.urls.load = urls.load;
+	useful.urls.save = urls.save;
+	useful.urls.encode = urls.encode;
+	useful.urls.decode = urls.decode;
+	useful.urls.replace = urls.replace;
+
+}(window.useful = window.useful || {}));
