784453e2660edb3f7c3c13b2e25c81786c679252
Maurice van Creij <maurice.vancreij@webqem.com>
2014-09-09 09:13:02 +1000

    Add support for require.js

--- README.md
+++ README.md
@@ -76,6 +76,15 @@ The following commands are available for development:
 + `grunt watch` - Continuously recompiles updated files during development sessions.
 + `grunt serve` - Serves the project on a temporary web server at http://localhost:8000/ .
 
+## How to test the script
+
+These test uses Selenium from http://docs.seleniumhq.org/
+
++ `npm install webdriverjs` - Installs the webdriver prerequisite.
++ `npm install mocha -g` - Installs the prerequisite test framework.
++ `java -jar /Applications/Selenium/selenium-server-standalone-2.42.2.jar` - Starts Selenium.
++ `mocha` - Runs the automated tests.
+
 ## License
 
 This work is licensed under a Creative Commons Attribution 3.0 Unported License. The latest version of this and other scripts by the same author can be found at http://www.woollymittens.nl/
---
+++ test/behaviours.js
@@ -0,0 +1,64 @@
+var webdriverjs = require('webdriverjs'),
+    assert = require('assert'),
+    usefulUrls = require('../src/js/useful-urls.js'),
+    path = 'http://local.woollymittens.nl/useful-urls/';
+
+describe('Expected behaviours of "useful-urls"', function(){
+
+    this.timeout(99999999);
+    var client = {};
+
+    before(function(){
+        client = webdriverjs.remote({ desiredCapabilities: {browserName: 'phantomjs'} });
+        client.init();
+    });
+
+    it('should import values using "load()" inside a browser', function(done){
+        client
+            .url(path)
+            .execute(function () {
+
+                return useful.urls.load('http://www.foo.bar/index.html?mystring=a&mynumber=1.5');
+
+            }, [], function (err, result) {
+
+                assert(err === null);
+                assert(result.value.mystring === 'a');
+                assert(result.value.mynumber === 1.5);
+
+            })
+            .call(done);
+    });
+
+    it('should import values using "load()" outside a browser', function(done){
+
+        var result = usefulUrls.load('http://www.foo.bar/index.html?mystring=a&mynumber=1.5');
+        assert(result.mystring === 'a');
+        assert(result.mynumber === 1.5);
+
+        done();
+    });
+
+    it('should export values using "save()" outside a browser', function(done){
+
+        var result = usefulUrls.save('http://www.foo.bar/index.html', { mystring : 'a', mynumber : 1.5 });
+        assert(result === 'http://www.foo.bar/index.html?mystring=a&mynumber=1.5');
+
+        done();
+    });
+
+    it('should replace a value using "replace()" outside a browser', function(done){
+
+        var result = usefulUrls.replace('http://www.foo.bar/index.html?mystring=a&mynumber=1.5', 'mystring', 'b');
+        assert(result === 'http://www.foo.bar/index.html?mystring=b&mynumber=1.5');
+
+        result = usefulUrls.replace('http://www.foo.bar/index.html?mystring=a&mynumber=1.5', 'mynumber', 2.0);
+        assert(result === 'http://www.foo.bar/index.html?mystring=a&mynumber=2');
+
+        done();
+    });
+
+    after(function(done) {
+        client.end(done);
+    });
+});
